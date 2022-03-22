b01fd45ee07346a142982244e45155fd62f355ef
RnzNDM <96422128+RnzNDM@users.noreply.github.com>
2022-03-22 21:03:42 +0100

    Commit on 22032022

---
+++ movies.py
@@ -0,0 +1,38 @@
+genres = ["horror", "thriller", "scifi", "comedy"]
+movie_library = [["horror", "The Shining", 8.4, 93, 146],
+                 ["horror", "The Ring", 7.1, 48, 115],
+                 ["horror", "The Purge", 5.7, 36, 85],
+                 ["horror", "The Conjuring", 7.5, 83, 112],
+                 ["thriller", "The Bone Collector", 6.7, 63, 118],
+                 ["thriller", "No Time To Die", 7.3, 88, 163],
+                 ["thriller", "Shutter Island", 8.2, 77, 138],
+                 ["thriller", "Se7en", 8.6, 95, 127],
+                 ["scifi", "Alien", 8.5, 94, 117],
+                 ["scifi", "Prometheus", 7.0, 68, 124],
+                 ["scifi", "Dune", 8.1, 90, 155],
+                 ["scifi", "The Matrix", 8.7, 85, 136],
+                 ["comedy", "The Intern", 7.1, 73, 121],
+                 ["comedy", "The Other Guys", 6.6, 60, 107],
+                 ["comedy", "Deuce Bigalow", 5.7, 42, 88],
+                 ["comedy", "Bad Boys II", 6.6, 78, 147]]
+                #Format is genre, title, IMDB score, RottenTomatoes score, duration in minutes
+
+#Linked Lists
+
+#Explore all
+
+#Explore by genre
+
+#Explore by duration
+
+#Explore by score
+
+#Sort by duration
+
+#Sort by score
+
+#Autocomplete
+
+#MovieNight flow
+print("Welcome to MovieNight! You can explore movies by genre, by duration, or explore all movies in our catalogue.\n")
+initial_choice = input("What would you like to explore:\nGenres: type G \nDurations: type D \nAll movies: type ALL\nYour choice: ")
